0ea3c00abef25bf76a2741e914a37ca7f83c76e7
zbob889 <zroberts889@gmail.com>
2022-06-02 17:11:35 +0200

    Finished.  I was able to write a single function that used filter and includes to do it in one line.  So fucking relieved.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(array, ...numbersToRemove) {
-    
-    let newArray = array.filter(number => number != numbersToRemove)
+
+    let newArray = array.filter(number => numbersToRemove.includes(number) === false)
 
     return newArray;
 
