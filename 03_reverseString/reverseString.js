182ebe20df8d7431a6e0ea253c264c09a77829e1
zbob889 <zroberts889@gmail.com>
2022-06-02 15:05:03 +0200

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString = ''
+
+    for(let i = string.length - 1; i >= 0; i--){
+        newString = newString += string[i]
+    }
+
+    return newString
 };
 
 // Do not edit below this line
