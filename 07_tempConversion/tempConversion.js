5b5b9ea285f14d523840898365b50dc7b0359e7b
zbob889 <zroberts889@gmail.com>
2022-06-03 14:05:20 +0200

    Finished.  Ezpz

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  newTemp = (fahrenheit - 32) * (5/9);
+  newTemp = +newTemp.toFixed(1);
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  newTemp = celcius * (9/5) + 32;
+  newTemp = +newTemp.toFixed(1);
+  return newTemp;
 };
 
 // Do not edit below this line
