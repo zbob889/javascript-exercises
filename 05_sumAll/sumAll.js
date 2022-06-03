ae66c0b46be769604ec78ee613d8b423c1c63ae3
zbob889 <zroberts889@gmail.com>
2022-06-03 13:22:36 +0200

    I got it working with numbers.  Now I just need to figure out how to do the error requirements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
 
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    let totalSum = 0;
+
+    for(let i = min; i < max + 1; i++){
+        totalSum += i;
+    };
+
+    return totalSum
 };
 
 // Do not edit below this line
