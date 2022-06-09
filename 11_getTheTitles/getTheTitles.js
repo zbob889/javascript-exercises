9048be492adee38d849ba8a3a87ffc8201f83d04
zbob889 <zroberts889@gmail.com>
2022-06-09 14:41:16 +0200

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let theTitles = books.map(book => book.title);
+    return theTitles;
 };
 
 // Do not edit below this line
