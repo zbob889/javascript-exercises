cdbeb32383f2638265bb951ef38857a62da0984a
zbob889 <zroberts889@gmail.com>
2022-06-09 13:22:56 +0200

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  const totalSum = numbers.reduce((a, b) => a + b, 0);
+  return totalSum
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const sum = numbers.reduce((a, b) => a * b);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let x = Math.pow(a, b);
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let x = 1
+  for(i = 1; i < a + 1; i++){
+    x = x * i
+  };
+  return x;
 };
 
 // Do not edit below this line
