e858461b6d60a6250d2a133f92f77d131c039137
zbob889 <zroberts889@gmail.com>
2022-06-09 15:26:08 +0200

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    people.forEach(function(person) {
+        if('yearOfDeath' in person === false){
+            person.yearOfDeath = 2022;
+        };
+    });
+
+    const oldestPeople = people.sort(function (a,b) {
+        const firstPerson = a.yearOfDeath - a.yearOfBirth;
+        const secondPerson = b.yearOfDeath - b.yearOfBirth;
+
+        if(firstPerson < secondPerson){
+            return 1;
+        } else {
+            return -1
+        };
+    });
+
+    return oldestPeople[0];
 
 };
 
